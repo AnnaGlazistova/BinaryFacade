33e4e8548aa6da2f8f9f74538e4b29b535875af9
Anna Glazistova <glazistovaao@gmail.com>
2022-07-18 12:08:17 +0800

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.mult("001","00111"));
+        System.out.println(bins.sum("001","00111"));
+    }
+}
